d31765523f21bef8a959fc367c146e0353609d5a
urm2015 <jackrabbitmoon@yahoo.com>
2015-09-21 20:37:46 -0500

    clean up code

--- project5/app.js
+++ project5/app.js
@@ -1,37 +1,39 @@
+//Project 5: Neighborhood Map
+//Robert Miller
+
 //**********Model Section********
 
 //Setup Map
 
 var mapOpt = {
-  zoom:16,
-  maxZoom:16,
-  minZoom:16,
-  center: new google.maps.LatLng(29.4268198, -984887071),
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  mapTypeControl: false,
-  streetViewControl: false,
-  panControl: false,
-  draggable: false,
-}
-
+  zoom : 16,
+  maxZoom : 16,
+  minZoom : 16,
+  center : new google.maps.LatLng(29.4268198, -98.4887071),
+  mapTypeId : google.maps.MapTypeId.ROADMAP,
+  mapTypeControl : false,
+  streetViewControl : false,
+  panControl : false,
+  draggable : false,
+};
 var map = new google.maps.Map(document.getElementById('map'), mapOpt);
-
 var iconURLPrefixRed = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 
-  // Define locations as an Array
+// Define locations as an Array
+
 var initialLocations = [
   {
     name : 'Tobin Center',
     title : 'Tobin Center for the Performing Arts',
     apiLink : '1',
-    extract : 'The 2100 seat venue was first built in 1926 as the San Antonio Municipal Auditorium ',
+    extract : 'The 2100 seat venue was first built in 1926',
     lat : '29.4306927',
     long : '-98.4886484',
-    marker: new google.maps.Marker({
-          position: new google.maps.LatLng(29.4306927, -98.4886484),
-          map: map,
-          icon: iconURLPrefixRed,
-        }),
+    marker : new google.maps.Marker({
+      position : new google.maps.LatLng(29.4306927, -98.4886484),
+      map : map,
+      icon : iconURLPrefixRed,
+    }),
   },
   {
     name : 'The Buckhorn',
@@ -40,37 +42,37 @@ var initialLocations = [
     extract : 'The museum was established in 1997',
     lat : '29.4261784',
     long : '-98.4889309',
-    marker: new google.maps.Marker({
-          position: new google.maps.LatLng(29.4261784, -98.4889309),
-          map: map,
-          icon: iconURLPrefixRed,
-        })
+    marker : new google.maps.Marker({
+      position : new google.maps.LatLng(29.4261784, -98.4889309),
+      map : map,
+      icon : iconURLPrefixRed,
+    }),
   },
   {
     name : 'The Majestic Theater',
-    title: 'Majestic Theatre (San Antonio)',
+    title : 'Majestic Theatre (San Antonio)',
     apiLink : '1',
     extract : 'The theatre seats 2,311 people and was built in 1929',
     lat : '29.42616',
     long : '-98.4905833',
-    marker: new google.maps.Marker({
-          position: new google.maps.LatLng(29.42616, -98.4905833),
-          map: map,
-          icon: iconURLPrefixRed,
-        })
+    marker : new google.maps.Marker({
+      position : new google.maps.LatLng(29.42616, -98.4905833),
+      map : map,
+      icon : iconURLPrefixRed,
+    }),
   },
   {
     name : 'The Alamo',
-    title: 'Alamo Mission in San Antonio',
+    title : 'Alamo Mission in San Antonio',
     apiLink : '1',
     extract : 'Founded in the eighteenth century as a Roman Catholic mission',
     lat : '29.4259671',
     long : '-98.4861419',
-    marker: new google.maps.Marker({
-          position: new google.maps.LatLng(29.4259671, -98.4861419),
-          map: map,
-          icon: iconURLPrefixRed,
-        }) 
+    marker : new google.maps.Marker({
+      position : new google.maps.LatLng(29.4259671, -98.4861419),
+      map : map,
+      icon : iconURLPrefixRed,
+    }),
   },
   {
     name : 'The Menger Hotel',
@@ -79,14 +81,14 @@ var initialLocations = [
     extract : 'The hotel was built in 1859',
     lat : '29.4268198',
     long : '-98.4887071',
-    marker: new google.maps.Marker({
-          position: new google.maps.LatLng(29.4248426, -98.486367),
-          map: map,
-          icon: iconURLPrefixRed,
-        })
+    marker : new google.maps.Marker({
+      position : new google.maps.LatLng(29.4248426, -98.486367),
+      map : map,
+      icon : iconURLPrefixRed,
+    }),
   }];
     
-  var Location = function(data) {
+var Location = function(data) {
   var self = this;
   this.name= ko.observable(data.name);
   this.title= ko.observable(data.title);
@@ -101,17 +103,18 @@ var initialLocations = [
 //*********ViewModel Section********
 
 var ViewModel = function() {
-    var self = this;
-    var infowindow = new google.maps.InfoWindow({
-      maxWidth: 160
-      });
+  var self = this;
+  var infowindow = new google.maps.InfoWindow({
+    maxWidth: 160
+  });
 
-//Make locations an observable array
+  //Make locations an observable array
 
-    this.locations = ko.observableArray([]);
+  this.locations = ko.observableArray([]);
 
-//Add elements to locations array
-  this.addElements= function(){
+  //Add elements to locations array
+
+  this.addElements = function(){
     initialLocations.forEach(function(place){
       self.locations().push(new Location(place));
     });
@@ -119,52 +122,43 @@ var ViewModel = function() {
 
   self.addElements();
 
-//Set marker animation to false
+  //Set marker animation to false
 
   this.animateFalse = function(){
-      self.locations().forEach(function(point){
-        point.marker().setAnimation(false);
-      });
-    };
+    self.locations().forEach(function(point){
+      point.marker().setAnimation(false);
+    });
+  };
 
-//Make wikipedia api call and store it in apiLink 
+  //Make wikipedia api call and store it in apiLink
 
-this.wikiData = function(){
-
-   self.locations().forEach(function(point){
-
-    var title = point.title();   
-
-    var titleUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + title;
-
-    var wikiRequestTimeout = setTimeout(function(){
+  this.wikiData = function(){
+    self.locations().forEach(function(point){
+      var title = point.title();
+      var titleUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + title;
+      var wikiRequestTimeout = setTimeout(function(){
         infowindow.setContent("failed to get wikipedia resources");
-    }, 8000);
+      }, 8000);
 
-    $.ajax({
-        url: titleUrl,
-        dataType: "jsonp",
-        success: function( response ) {
-            var titleid = Object.keys(response.query.pages)[0];           
-                point.apiLink = 'http://en.wikipedia.org/?curid=' + titleid;
-            },
-
-            
+      $.ajax({
+          url : titleUrl,
+          dataType : "jsonp",
+          success : function(response) {
+            var titleid = Object.keys(response.query.pages)[0];
+              point.apiLink = 'http://en.wikipedia.org/?curid=' + titleid;
+          },
       });
       clearTimeout(wikiRequestTimeout);
-    });  
+    });
   };
+
   self.wikiData();
 
-
-
-//Add click event to marker and info window
+  //Add click event to marker and info window
 
   this.addClick = function(){  
     self.locations().forEach(function(place){
-    
-      marker = place.marker();
-
+      var marker = place.marker();
       google.maps.event.addListener(marker, 'click', (function(marker, place) {
         return function() {
           self.animateFalse();
@@ -175,58 +169,50 @@ this.wikiData = function(){
       })(marker, place));
     });
   };
+
   self.addClick();
 
-//Set all markers visible
+  //Set all markers visible
 
   this.setMarker = function(){
-      self.locations().forEach(function(point){
-      point.marker().setVisible(true);
-    })
-  }; 
+    self.locations().forEach(function(point){
+    point.marker().setVisible(true);
+    });
+  };
 
-//Set list view to be clicakble
+  //Set list view to be clicakble
 
-this.currentMarker = ko.observable( this.locations()[0] );
+  this.currentMarker = ko.observable(this.locations()[0]);
+  this.setClickedMarker = function(clickedMarker) {
+    self.currentMarker(clickedMarker);
+    var name = this.name();
+    var apiLink = this.apiLink;
+    var marker = this.marker();
+    var extract = this.extract();
+    self.animateFalse();
+    infowindow.setContent('<a href="' + apiLink + '">' +  name + '</a>' +"<br />" + extract);
+    infowindow.open(map, marker);
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  };
 
-this.setClickedMarker = function(clickedMarker) {
-  self.currentMarker(clickedMarker);
+  //Center map on marker locations
 
-  var name = this.name();
-  var apiLink = this.apiLink;
-  var marker = this.marker();
-  var extract = this.extract();
-
-  self.animateFalse();
-  infowindow.setContent('<a href="' + apiLink + '">' +  name + '</a>' +"<br />" + extract);
-  infowindow.open(map, marker);
-  marker.setAnimation(google.maps.Animation.BOUNCE);
-};
-
-//Center map on marker locations
-
-this.autoCenter = function(data) {
-
-  //  Create a new viewpoint bound
-  var bounds = new google.maps.LatLngBounds();
-
-    self.locations().forEach(function(point){ 
+  this.autoCenter = function(data) {
+    //  Create a new viewpoint bound
+    var bounds = new google.maps.LatLngBounds();
+    self.locations().forEach(function(point){
       bounds.extend(point.marker().position);
     });
-    //  Fit these bounds to the map
+    //Fit these bounds to the map
     map.fitBounds(bounds);
-};
-    
-self.autoCenter();
+  };
 
+  self.autoCenter();
 
-    
+  //Search and filter the list and markers
 
-//Search and filter the list and markers
-  
   this.searchFilter = function(){
     self.query = ko.observable('');
-
     self.search = ko.computed(function(){
       self.setMarker();
       self.animateFalse();
@@ -235,12 +221,12 @@ self.autoCenter();
           if (point.name().toLowerCase().indexOf(self.query().toLowerCase()) >=0){
               point.marker().setVisible(true);
               infowindow.close(map, point.marker());
-          } else { 
+          } else {
               point.marker().setVisible(false);
               infowindow.close(map, point.marker());
             }
-          })    
-        return place.name().toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+        });
+        return place.name().toLowerCase().indexOf(self.query().toLowerCase()) >=0;
       });
     });
   };
